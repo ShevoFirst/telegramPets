5699ace896373aaebf1b39995afa24be00d969b8
ShevoFirst <play.job@yandex.ru>
2023-07-17 20:58:21 +0400

    initial commit

---
+++ src/main/java/pro/sky/telegrampets/TelegramPetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrampets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/telegrampets/TelegramPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrampets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
