47c671ca1dc1cbe38b8431ef73b429ba55ffa3d7
sonic <evtepo@gmail.com>
2023-08-19 10:15:40 +0300

    Рефакторинг

---
+++ src/main/java/pro/sky/telegrampets/service/ReportService.java
@@ -0,0 +1,9 @@
+package pro.sky.telegrampets.service;
+
+import pro.sky.telegrampets.model.Report;
+
+public interface ReportService {
+    Report reportAdd(Report report);
+
+    void updateReport(Report report);
+}
