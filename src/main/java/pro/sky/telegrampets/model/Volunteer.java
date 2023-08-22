4a5a6580c544b7f8f2ae963eb9e55e81bc722815
79650 <last137954682@gmail.com>
2023-08-22 14:12:21 +0300

    api for the user and tests

--- src/main/java/pro/sky/telegrampets/model/Volunteer.java
+++ src/main/java/pro/sky/telegrampets/model/Volunteer.java
@@ -1,11 +1,6 @@
 package pro.sky.telegrampets.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 @Entity
 @Table(name = "volunteers")
